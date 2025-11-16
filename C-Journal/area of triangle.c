140a07ba62303938a00acf02b3ac63be8476ba66
Sulav Khatri <sulav.k.chhetri@gmail.com>
2025-11-16 12:27:47 +0545

    Create area of triangle.c

---
+++ C-Journal/area of triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int b, h, area;
+  printf("Enter the base and height;");
+  scanf("%d%d", &b, &h);
+  area = 0.5*(b*h);
+  printf("The area is %d, area);
+}    
+    
