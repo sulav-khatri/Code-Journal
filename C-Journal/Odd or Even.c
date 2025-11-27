cea872e2361a104dbb9c9a6c668b7a74a2189446
Sulav Khatri <sulav.k.chhetri@gmail.com>
2025-11-27 14:20:23 +0545

    Correct odd/even output messages in C program
    Fix incorrect output messages for odd/even check.

---
+++ C-Journal/Odd or Even.c
@@ -0,0 +1,17 @@
+#inclue <stdio.h>
+
+int main()
+{
+  int a;
+  printf("Enter a number : ");
+  scanf("%d", &a);
+
+  if (a%2==0)
+  {
+    printf("The given number is odd.\n");
+  }
+  else
+  {
+    printf("The given number is even.\n");
+  }
+}
