45b3e98c5e40b1eabbe6938428704dfd898c0f13
Sulav Khatri <sulav.k.chhetri@gmail.com>
2025-11-27 14:25:20 +0545

    Implement divisibility check for 5 and 6
    Fix the logic for checking divisibility by 5 and 6.

---
+++ C-Journal/Divisble or Not.c
@@ -0,0 +1,19 @@
+//Program to check id the number is divisibe by 5 or 6 or not
+#include <stdio.h>
+
+int main();
+{
+  int a;
+  printf("Enter a number : ");
+  scanf("%d", &a);
+
+  if (a%5&&==6)
+  {
+    printf("The given number is divisible by both 5 and 6.\n");
+  }
+
+  else 
+  {
+    printf("The given number is not divisible by both 5 and 6.\n");
+  }
+}
